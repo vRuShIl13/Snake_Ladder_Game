7d913d128315536b913d6cb4df233f1e2cc0e6cd
DESKTOP-EVRA9NN\vrush <vrushil13@gmail.com>
2022-02-07 11:55:39 -0600

    Initial commit Started coding in the cpp the cpp file

---
+++ cmake-build-debug/CMakeFiles/Snake_Ladder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake_Ladder.dir/main.cpp.obj"
+  "Snake_Ladder.exe"
+  "Snake_Ladder.exe.manifest"
+  "Snake_Ladder.pdb"
+  "libSnake_Ladder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake_Ladder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void draw_line(int n,char ch);
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
